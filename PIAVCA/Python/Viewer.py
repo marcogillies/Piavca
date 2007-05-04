174ef5efe91f5ff736c423b59fc0d202c6141f64
gueedz <gueedz@ec56aa41-f30e-0410-ba60-f254d220f853>
2007-05-04 16:42:07 +0000

    added a simple viewer script
    git-svn-id: https://piavca.svn.sourceforge.net/svnroot/piavca/trunk@115 ec56aa41-f30e-0410-ba60-f254d220f853

---
+++ PIAVCA/Python/Viewer.py
@@ -0,0 +1,65 @@
+
+import sys
+import os
+import time
+
+
+import Piavca
+Piavca.PiavcaGlut.init()
+import wx
+from wx.lib.dialogs import *
+
+import thread
+
+app = wx.PySimpleApp()
+	
+if len(sys.argv) > 1:
+	path = sys.argv[1]
+else:
+	dialog_return = openFileDialog ()
+	path = dialog_return.paths[0]
+
+print path
+pathend = path.rfind("\\")
+if pathend < -1:
+	pathend = path.rfind("/")
+if pathend < -1:
+	filename = path
+else:
+	filename = path[pathend+1:]
+	path = path[:pathend+1]
+	if path != "":
+		os.chdir(path)
+print filename, path
+if filename[-4:] == ".cfg":
+	filename = filename[:-4]
+print filename
+	
+try:
+	Piavca.JointNames.importJointNames("Joints.txt")
+except IOError:
+	print "could not open Joints file Joints.txt, will probably work anyway"
+try:
+	Piavca.JointNames.importExpressionNames("Expressions.txt")
+except IOError:
+	print "could not open Expressions file Expressions.txt, will probably work anyway"
+
+avatar = Piavca.Avatar(filename)
+
+if len(sys.argv) > 2:
+	motion_name = sys.argv[2]
+else:
+	motnames = Piavca.Core.getCore().getMotionNames()
+	print motnames
+	dialog_return = singleChoiceDialog (lst=["None"]+motnames)
+	motion_name = dialog_return.selection
+
+#motion_name = "biped_mocap_final"
+mot = Piavca.Core.getCore().getMotion(motion_name)
+if mot != None:
+	mot.Reference()
+	avatar.play_motion(Piavca.LoopMotion(mot))
+		
+		
+Piavca.PiavcaGlut.start()
+	
