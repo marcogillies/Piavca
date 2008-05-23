540d81f8fbb19c10f2afab5479a127871512a8e4
gueedz <gueedz@ec56aa41-f30e-0410-ba60-f254d220f853>
2008-05-23 10:17:05 +0000

    fixed a version problem with the visual studio projects and added the PiavcaDesigner main script that I forgot to add earlier
    git-svn-id: https://piavca.svn.sourceforge.net/svnroot/piavca/trunk@206 ec56aa41-f30e-0410-ba60-f254d220f853

---
+++ PIAVCA/Python/PiavcaDesigner.py
@@ -0,0 +1,25 @@
+
+
+import Piavca
+import Piavca.JointNames
+Piavca.JointNames.loadDefaults()
+
+
+from Piavca.PiavcaDesigner import PiavcaDesigner
+
+app = Piavca.getWXApp()
+
+	
+try:
+	Piavca.JointNames.importJointNames("Joints.txt")
+except IOError:
+	print "could not open Joints file Joints.txt, will probably work anyway"
+try:
+	Piavca.JointNames.importExpressionNames("Expressions.txt")
+except IOError:
+	print "could not open Expressions file Expressions.txt, will probably work anyway"
+	
+	
+pd = PiavcaDesigner()
+	
+app.MainLoop()
